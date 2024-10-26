7876eaaef87f08febf837c9112365153f22c6183
JGutierrez2294 <isnp115024@ugb.edu.sv>
2024-10-26 17:45:41 -0600

    Last part

--- ISNP115024_ISNP201823_ISNP205923_Bloque2/Form1.vb
+++ ISNP115024_ISNP201823_ISNP205923_Bloque2/Form1.vb
@@ -2,4 +2,8 @@
     Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles grbNavegacion.Enter
 
     End Sub
+
+    Private Sub Clientes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
 End Class
